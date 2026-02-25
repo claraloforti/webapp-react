9d689964051159dce3622b6f8dd3ad80bc1af4ba
Clara Loforti <clara.loforti@gmail.com>
2026-02-25 18:06:20 +0100

    fix comment

--- src/components/MovieCardReview.jsx
+++ src/components/MovieCardReview.jsx
@@ -6,7 +6,7 @@ const MovieCardReview = (props) => {
     const { name, vote, text } = props.reviewProp
 
     return (
-        // Contenuti momentanei della review
+        // Contenuti della review
         <div className="card mb-4">
             <div className="card-body">
                 <p className="card-text">{text}</p>
