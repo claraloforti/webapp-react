a7daa7f97893cdd61543e2c6c3630f125873c823
Clara Loforti <clara.loforti@gmail.com>
2026-02-26 16:27:21 +0100

    mostro il voto delle reviews in stelle

--- src/components/MovieCardReview.jsx
+++ src/components/MovieCardReview.jsx
@@ -5,12 +5,15 @@ const MovieCardReview = (props) => {
     // Destrutturo la prop review ricevuta dal padre MovieDetailPage
     const { name, vote, text } = props.reviewProp
 
+    // Mostro il voto in stelle
+    const stars = "★".repeat(vote) + "☆".repeat(5 - vote);
+
     return (
         // Contenuti della review
         <div className="card mb-4">
             <div className="card-body">
                 <p className="card-text">{text}</p>
-                <strong>Vote: {vote}</strong>
+                <strong>Vote: <span className="text-warning">{stars}</span></strong>
                 <address><i>By {name}</i></address>
             </div>
         </div>
