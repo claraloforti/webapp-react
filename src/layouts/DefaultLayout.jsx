5a162951fadf50a527f7fc5ecf08ce622a4f52f5
Clara Loforti <clara.loforti@gmail.com>
2026-02-26 15:37:08 +0100

    aggiungo il loader nel layout

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,14 +1,21 @@
 import { Outlet } from "react-router-dom";
-
+import { useGlobal } from "../contexts/GlobalContext";
+import Loader from "../components/Loader";
 import MainHeader from "../components/MainHeader";
 
 const DefaultLayout = () => {
+
+    // Recupero stato globale del loader dal contesto
+    const { isLoading } = useGlobal();
+
     return (
         <>
             <MainHeader /> {/* Componente sempre visibile */}
             <main className="container">
                 <Outlet /> {/* Segnaposto per il contenuto dinamico in base alla rotta corrente */}
             </main>
+            {/* Se isLoading è true, mostra il Loader */}
+            {isLoading && <Loader />}
         </>
     )
 }
